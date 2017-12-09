aec20cff2fd22bb67d96ec6f747cccef76e086bc
Faïssal <faissal.hattou@gmail.com>
2017-12-09 16:12:05 +0100

    ADD: login/logout working. TODO: front with React

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,20 +1,28 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
-    res.render('login');
+  res.render('login');
 });
 
 // auth logout
 router.get('/logout', (req, res) => {
-    // handle with passport
-    res.send('logging out');
+  // we logout with the logout() passport method
+  req.logout();
+  res.redirect('/');
 });
 
 // auth with google
-router.get('/google', (req, res) => {
-    // handle with passport
-    res.send('logging in with google');
+router.get('/google', passport.authenticate('google', {
+  // we want the profile informations about the user
+  scope: ['profile'],
+}));
+
+// callback route for google to redirect to
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  // res.send(req.user);
+  res.redirect('/profile/');
 });
 
 module.exports = router;
